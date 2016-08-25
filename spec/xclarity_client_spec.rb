ecbe04b2d91d16bbc087ecfe1d6053b9f7460662
Julian Cheal <jcheal@redhat.com>
2016-08-25 23:17:58 +0100

    Added stup methods for all the Virtual appliance management API calls

--- spec/xclarity_client_spec.rb
+++ spec/xclarity_client_spec.rb
@@ -1,6 +1,17 @@
 require 'spec_helper'
 
 describe XClarityClient do
+
+  before :all do
+    conf = XClarityClient::Configuration.new(
+      :username => 'admin',
+      :password => 'pass',
+      :host     => 'http://example.com'
+    )
+
+    @virtual_appliance = XClarityClient::VirtualApplianceManagement.new(conf)
+  end
+
   it 'has a version number' do
     expect(XClarityClient::VERSION).not_to be nil
   end
@@ -8,25 +19,54 @@ describe XClarityClient do
   describe 'GET /aicc' do
     it 'should respond with information about the Lenovo XClarity Administrator' do
 
-      conf = XClarityClient::Configuration.new(
-        :username => 'admin',
-        :password => 'pass',
-        :host     => 'http://example.com'
-      )
-
-      virtual_appliance = XClarityClient::VirtualApplianceManagement.new(conf)
-
-      response = virtual_appliance.configuration_settings
+      response = @virtual_appliance.configuration_settings
 
       expect(response.status).to eq(200)
     end
   end
 
-  # TODO: Actually create real tests!
   describe 'GET /aicc/network/ipdisable' do
     it 'should respond with the IPv6 and IPv6 addresses enablement state.' do
-      response = 200
-      expect(response).to eq(200)
+
+      response = @virtual_appliance.ip_enablement_state
+
+      expect(response.status).to eq(200)
+    end
+  end
+
+  describe 'GET /aicc/network/host' do
+    it 'should respond with the XClarity Administrator host settings.' do
+
+      response = @virtual_appliance.host_settings
+
+      expect(response.status).to eq(200)
+    end
+  end
+
+  describe 'GET /aicc/network/interfaces/{interface}' do
+    it 'should respond with information about a specific network interface.' do
+
+      response = @virtual_appliance.network_interface_settings("eth0")
+
+      expect(response.status).to eq(200)
+    end
+  end
+
+  describe 'GET /aicc/network/routes' do
+    it 'should respond with all XClarity Administrator routes.' do
+
+      response = @virtual_appliance.route_settings
+
+      expect(response.status).to eq(200)
+    end
+  end
+
+  describe 'GET /aicc/network/host' do
+    it 'should respond with all XClarity Administrator subscriptions.' do
+
+      response = @virtual_appliance.subscriptions
+
+      expect(response.status).to eq(200)
     end
   end
 end
