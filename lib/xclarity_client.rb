ba06f7f8a47bde8ae5f9aeb52d7765bcbb8ce974
Julian Cheal <jcheal@redhat.com>
2016-08-25 15:08:21 +0100

    Added basic structure for a ruby gem

---
+++ lib/xclarity_client.rb
@@ -0,0 +1,5 @@
+require "xclarity_client/version"
+
+module XclarityClient
+  # Your code goes here...
+end
---
+++ spec/xclarity_client_spec.rb
@@ -0,0 +1,11 @@
+require 'spec_helper'
+
+describe XclarityClient do
+  it 'has a version number' do
+    expect(XclarityClient::VERSION).not_to be nil
+  end
+
+  it 'does something useful' do
+    expect(false).to eq(true)
+  end
+end
