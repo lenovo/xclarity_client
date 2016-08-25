06304f5e33727e99b1d923151cd40a05129fa8b2
Julian Cheal <jcheal@redhat.com>
2016-08-25 23:17:58 +0100

    Added stup methods for all the Virtual appliance management API calls

--- lib/xclarity_client/virtual_appliance_management.rb
+++ lib/xclarity_client/virtual_appliance_management.rb
@@ -1,18 +1,76 @@
 module XClarityClient
   class VirtualApplianceManagement < XClarityBase
 
-    BASE_URI = '/aicc'
+    BASE_URI          = '/aicc'.freeze
+    NETWORK_URI       = '/network'.freeze
+    IPDISABLE_URI     = '/ipdisable'.freeze
+    HOST_URI          = '/host'.freeze
+    INTERFACES_URI    = '/interfaces'.freeze
+    ROUTES_URI        = '/routes'.freeze
+    SUBSCRIPTIONS_URI = '/subscriptions'.freeze
 
     def initialize(conf)
       super(conf, BASE_URI)
     end
 
     def configuration_settings
-      response = @conn.get
+      response = connection
       response
     end
 
-    def configuration_settings=
+    def configuration_settings=()
     end
+
+    def ip_enablement_state
+      uri = BASE_URI+NETWORK_URI+IPDISABLE_URI
+      response = connection(uri)
+      response
+    end
+
+    def ip_enablement_state=()
+
+    end
+
+    def host_settings
+      uri = BASE_URI+NETWORK_URI+IPDISABLE_URI
+      response = connection(uri)
+      response
+    end
+
+    def host_settings=()
+
+    end
+
+    def network_interface_settings(interface)
+      uri = BASE_URI+NETWORK_URI+INTERFACES_URI+"/#{interface}"
+      response = connection(uri)
+      response
+    end
+
+    def host_settings=()
+
+    end
+
+    def route_settings
+      uri = BASE_URI+NETWORK_URI+ROUTES_URI
+      response = connection(uri)
+      response
+    end
+
+    def route_settings=()
+
+    end
+
+    def subscriptions
+      uri = BASE_URI+SUBSCRIPTIONS_URI
+      response = connection(uri)
+      response
+    end
+
+    def subscriptions=()
+
+    end
+
+
   end
 end
--- lib/xclarity_client/xclarity_base.rb
+++ lib/xclarity_client/xclarity_base.rb
@@ -6,15 +6,21 @@ module XClarityClient
     attr_reader :conn
 
     def initialize(conf, uri)
-      connection(conf, uri)
+      connection_builder(conf, uri)
     end
 
-    def connection(conf, uri)
+    def connection_builder(conf, uri)
       @conn = Faraday.new(:url => conf.host + uri) do |faraday|
         faraday.request  :url_encoded             # form-encode POST params
         faraday.response :logger                  # log requests to STDOUT
         faraday.adapter  Faraday.default_adapter  # make requests with Net::HTTP
       end
+    end
+
+    private
+
+    def connection(uri = "", options = {})
+      @conn.get(uri)
     end
   end
 end
