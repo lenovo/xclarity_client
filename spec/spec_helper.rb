2b0d9629d7b4d50e870eefac38072ad5932529ff
Julian Cheal <jcheal@redhat.com>
2016-08-25 19:04:45 +0100

    Added API Blueprints for API test mocking
    Using `apib/mock_server` gem to create a mock server based on API
    Blueprints files (which are Markdown files to describe an API)
    
    This not only allows us to generate API documentation, but also
    locally test our client.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,2 +1,14 @@
 $LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
 require 'xclarity_client'
+require 'apib/mock_server'
+require 'webmock/rspec'
+
+base_url = "http://example.com"
+blueprint = Pathname(__dir__).join("..", "docs/apib", "aicc.apib").read
+app = Apib::MockServer.new(base_url, blueprint)
+
+RSpec.configure do |config|
+  config.before do
+    stub_request(:any, /#{base_url}/).to_rack(app)
+  end
+end
