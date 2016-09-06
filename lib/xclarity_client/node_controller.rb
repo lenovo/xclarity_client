b4f9c429da9e0e92d193ffe9a4318381c1afa166
Odravison Amaral <oamaraljr@gmail.com>
2016-09-06 16:28:50 -0300

    [/Node] Commit to give access to colaborator Maysa and Andrey.

--- lib/xclarity_client/node_controller.rb
+++ lib/xclarity_client/node_controller.rb
@@ -14,26 +14,40 @@ module XClarityClient
       end
     end
 
-    def get_object_nodes(uuids)
-      response = connection(BASE_URI + "/" + uuids.join(","))
+    def get_object_nodes(uuids, includeAttributes, excludeAttributes)
+
+      response = if not includeAttributes.nil?
+                   get_object_nodes_include_attributes(uuids, includeAttributes)
+                 elsif not excludeAttributes.nil?
+                   get_object_nodes_exclude_attributes(uuids, excludeAttributes)
+                 elsif not uuids.nil?
+                   connection(BASE_URI + "/" + uuids.join(","))
+                 else
+                   connection(BASE_URI)
+                 end
+
       response.body[:nodeList].map do |node|
         Node.new node
       end
     end
 
+    private
+
     def get_object_nodes_exclude_attributes(uuids, attributes)
-      response = connection(BASE_URI + "/" + uuids.join(",") + "?excludeAttributes" + attributes.join(","))
-      response.body[:nodeList].map do |node|
-        Node.new node
-      end
+
+      response = if not uuids.nil?
+                   connection(BASE_URI + "/" + uuids.join(",") + "?excludeAttributes" + attributes.join(","))
+                 else
+                   connection(BASE_URI + "?excludeAttributes" + attributes.join(","))
+                 end
     end
 
     def get_object_nodes_include_attributes(uuids, attributes)
-      response = connection(BASE_URI + "/" + uuids.join(",")  + "?excludeAttributes" + attributes.join(","))
-      response.body[:nodeList].map do |node|
-        Node.new node
-      end
+      response = if not uuids.nil?
+                   connection(BASE_URI + "/" + uuids.join(",") + "?includeAttributes" + attributes.join(","))
+                 else
+                   connection(BASE_URI + "?includeAttributes" + attributes.join(","))
+                 end
     end
-
   end
 end
