0d5d4afc4a0f78528d77b266f0ee448805b02805
gabrielvinha <gabriel.vinha@ccc.ufcg.edu.br>
2016-09-22 17:12:07 -0300

    Adding test for chassi with uuid

--- example/simple.rb
+++ example/simple.rb
@@ -14,9 +14,11 @@ client = XClarityClient::Client.new(conf)
 
 @includeAttributes = %w(accessState activationKeys)
 @excludeAttributes = %w(accessState activationKeys)
-# @uuidArray = client.discover_nodes.map { |node| node.uuid  }
+@uuidArray = client.discover_chassis.map { |node| node.uuid  }
 
-# puts client.discover_chassis
-puts client.fetch_chassis(@uuidArray, @excludeAttributes, nil)
+puts @uuidArray[0]
+# response = client.fetch_chassis(@uuidArray, @includeAttributes, nil)
+# puts response[0]
+# not_nil = response[0].send(@includeAttributes[0])
 # puts client.fetch_chassis(@uuidArray, nil, @excludeAttributes)
 # puts client.discover_nodes[0]
