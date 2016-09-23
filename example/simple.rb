af19496b4ffe0084f6d1c232de3f11547b125bbf
Amanda Souza <amanda.minueza@gmail.com>
2016-09-23 08:46:16 -0300

    [/Switches] Changing simple.rb

--- example/simple.rb
+++ example/simple.rb
@@ -6,19 +6,10 @@ conf = XClarityClient::Configuration.new(
   :host     => 'http://127.0.0.1:3000'
 )
 
-# virtual_appliance = XClarityClient::VirtualApplianceManagement.new(conf)
-
-# puts virtual_appliance.configuration_settings
-
 client = XClarityClient::Client.new(conf)
 
 
-@includeAttributes = %w(accessState activationKeys)
-@excludeAttributes = %w(accessState activationKeys)
-# @uuidArray = client.discover_nodes.map { |node| node.uuid  }
-
-# puts client.discover_chassis
-# puts client.fetch_switches
-# puts client.fetch_nodes(@uuidArray, nil, @excludeAttributes)
-# puts client.fetch_chassis(@uuidArray, nil, @excludeAttributes)
-puts client.fetch_switches(@uuidArray, nil, @excludeAttributes)
+# @includeAttributes = %w(accessState activationKeys)
+# @excludeAttributes = %w(accessState activationKeys)
+#
+# puts client.fetch_switches(@uuidArray, nil, @excludeAttributes)
