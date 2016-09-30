fc78ee6a4407d91c28537d16dc4d42d9e3047507
Odravison Amaral <odravison.amaral@dce.ufpb.br>
2016-09-30 11:52:07 -0300

    [/nodes] Fixing API BluePrint mock and fixing some worngs tests

--- spec/xclarity_client_spec.rb
+++ spec/xclarity_client_spec.rb
@@ -7,21 +7,10 @@ describe XClarityClient do
     conf = XClarityClient::Configuration.new(
     :username => 'admin',
     :password => 'pass',
-    :host     => 'http://127.0.0.1:3000'
-    )
-
-    conf_blueprint = XClarityClient::Configuration.new(
-    :username => 'admin',
-    :password => 'pass',
     :host     => 'http://example.com'
     )
 
-    @virtual_appliance = XClarityClient::VirtualApplianceManagement.new(conf_blueprint)
-    @client = XClarityClient::Client.new(conf)
-
-    @includeAttributes = %w(accessState activationKeys)
-    @excludeAttributes = %w(accessState activationKeys)
-    @uuidArray = @client.discover_nodes.map { |node| node.uuid  }
+    @virtual_appliance = XClarityClient::VirtualApplianceManagement.new(conf)
   end
 
   it 'has a version number' do
@@ -80,86 +69,5 @@ describe XClarityClient do
 
       expect(response.status).to eq(200)
     end
-  end
-
-  describe 'GET /nodes' do
-
-    it 'should respond with an array' do
-      expect(@client.discover_nodes.class).to eq(Array)
-    end
-
-    it 'the response must have one or more nodes' do
-      expect(@client.discover_nodes).not_to be_empty
-    end
-
-  end
-
-  describe 'GET /nodes/UUID' do
-
-    it 'with includeAttributes' do
-      response = @client.fetch_nodes([@uuidArray[0]], @includeAttributes,nil)
-      response.map do |node|
-        @includeAttributes.map do |attribute|
-          expect(node.send(attribute)).not_to be_nil
-        end
-      end
-
-    end
-
-    it 'with excludeAttributes' do
-      response = @client.fetch_nodes([@uuidArray[0]], nil, @excludeAttributes)
-      response.map do |node|
-        @excludeAttributes.map do |attribute|
-          expect(node.send(attribute)).to be_nil
-        end
-      end
-    end
-  end
-
-  describe 'GET /nodes/UUID,UUID,...,UUID' do
-
-    it 'to multiples uuid, should return two or more nodes' do
-      uuidArray = @client.discover_nodes.map { |node| node.uuid  }
-      expect(uuidArray.length).to be >= 2
-    end
-
-    it 'with includeAttributes' do
-      response = @client.fetch_nodes(@uuidArray, @includeAttributes,nil)
-      response.map do |node|
-        @includeAttributes.map do |attribute|
-          expect(node.send(attribute)).not_to be_nil
-        end
-      end
-    end
-
-    it 'with excludeAttributes' do
-      response = @client.fetch_nodes(@uuidArray, nil, @excludeAttributes)
-      response.map do |node|
-        @excludeAttributes.map do |attribute|
-          expect(node.send(attribute)).to be_nil
-        end
-      end
-    end
-  end
-
-  describe 'GET /nodes' do
-
-    it 'with includeAttributes' do
-      response = @client.fetch_nodes(nil,@includeAttributes,nil)
-      response.map do |node|
-        @includeAttributes.map do |attribute|
-          expect(node.send(attribute)).not_to be_nil
-        end
-      end
-    end
-    it 'with excludeAttributes' do
-      response = @client.fetch_nodes(nil,nil,@excludeAttributes)
-      response.map do |node|
-        @excludeAttributes.map do |attribute|
-          expect(node.send(attribute)).to be_nil
-        end
-      end
-    end
-
   end
 end
