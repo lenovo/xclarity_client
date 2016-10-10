5eeae4c92c6b594247095b47648352a7822e894e
Odravison Amaral <odravison.amaral@dce.ufpb.br>
2016-10-10 15:30:16 -0300

    Fixing some wrong code on simple.rb to give access colaborator Daniel Madruga.

--- example/simple.rb
+++ example/simple.rb
@@ -1,9 +1,10 @@
 require 'xclarity_client'
 
 conf = XClarityClient::Configuration.new(
-  :username => 'admin',
-  :password => 'pass',
-  :host     => 'http://127.0.0.1:3000'
+  :username => 'USER-HERE',
+  :password => 'PASSWORD-HERE',
+  :host     => 'HOST_DOMAIN_HERE',
+  :auth_type => 'TYPE_OF_AUTHENTICATION'
 )
 
 # virtual_appliance = XClarityClient::VirtualApplianceManagement.new(conf)
@@ -12,14 +13,4 @@ conf = XClarityClient::Configuration.new(
 
 client = XClarityClient::Client.new(conf)
 
-@includeAttributes = %w(accessState activationKeys)
-@excludeAttributes = %w(accessState activationKeys)
-@uuidArray = client.discover_chassis.map { |node| node.uuid  }
-
-puts @uuidArray[0]
-response = client.fetch_chassis(@uuidArray, @includeAttributes, nil)
-puts response[0]
-not_nil = response[0].send(@includeAttributes[0])
-puts not_nil
-# puts client.fetch_chassis(@uuidArray, nil, @excludeAttributes)
-# puts client.discover_nodes[0]
+puts client.discover_chassis
