7f0c6206209c529f1a6c7643c17ca2d0b61b1223
Tercio de Melo <terciodemelo@gmail.com>
2016-11-01 11:25:02 -0300

    Removes null bytes from string values
    As it seems, LXCA clusters may return string values with '\u0000'
    in them, which is interpreted by Postgres as a null byte

--- lib/xclarity_client/xclarity_resource.rb
+++ lib/xclarity_client/xclarity_resource.rb
@@ -3,6 +3,7 @@ module XClarityClient
     def build_resource(attributes)
       attributes.each do |key, value|
         begin
+          value = value.gsub("\u0000", '') if value.is_a?(String)
           send("#{key}=", value)
         rescue
           $log.warn("UNEXISTING ATTRIBUTES FOR #{self.class}: #{key}") unless defined?(Rails).nil?
