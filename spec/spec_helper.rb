999c7493c47d510277d383cbb1bd5aeb8d20a44b
Tercio de Melo <terciodemelo@gmail.com>
2016-11-04 13:32:10 -0300

    Renames default environment variables for LXCA prefix
    This enhances integration with other systems

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -5,7 +5,7 @@ require 'webmock/rspec'
 
 
 base_url = "http://example.com"
-# This environment variables must to be defined
+# These environment variables must be defined
 ENV['LXCA_USERNAME']   ||= ''
 ENV['LXCA_PASSWORD']   ||= ''
 ENV['LXCA_HOST']       ||= base_url
