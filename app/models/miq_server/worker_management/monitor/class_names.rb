9cd79e913e8d59975332e4435875f67a56a4d7f0
Tercio de Melo <terciodemelo@gmail.com>
2017-01-19 16:31:23 -0300

    [LXCA Events] Registers LXCA EventCatcher

--- app/models/miq_server/worker_management/monitor/class_names.rb
+++ app/models/miq_server/worker_management/monitor/class_names.rb
@@ -58,6 +58,7 @@ module MiqServer::WorkerManagement::Monitor::ClassNames
     ManageIQ::Providers::StorageManager::CinderManager::EventCatcher
     ManageIQ::Providers::Vmware::InfraManager::EventCatcher
     ManageIQ::Providers::Vmware::CloudManager::EventCatcher
+    ManageIQ::Providers::Lenovo::PhysicalInfraManager::EventCatcher
     MiqEventHandler
     MiqGenericWorker
     MiqNetappRefreshWorker
