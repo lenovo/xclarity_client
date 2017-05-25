86284032f47b7309307e0d50614f7b86881c9e17
Walter Alves <walter.arruda.alves@gmail.com>
2017-05-25 13:35:22 -0300

    Add log strategy
    Add a strategy to log the data fetch process

---
+++ lib/utils/logger.rb
@@ -0,0 +1,29 @@
+require 'logger'
+
+module XClarityClient
+
+  class XClarityLogger
+    
+    def initialize(global_log=nil)
+      @log = $lenovo_log ||= $log ||= global_log
+
+      if not @log
+        file = File.open('lenovo.log', 'a+')
+        file.sync = true
+        @log = Logger.new file
+      end
+
+    end
+
+    def info(header, msg)
+      @log.level = Logger::DEBUG
+      @log.info "[#{header}] - #{msg}"
+    end
+
+    def error(header, msg)
+      @log.level = Logger::ERROR
+      @log.error "[#{header} - #{msg}]"
+    end
+  end
+
+end
