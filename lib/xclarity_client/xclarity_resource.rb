de51ab74f2cdffa96b4df022531c323335c2832d
Maysa Macedo <maysa.souza@ccc.ufcg.edu.br>
2017-06-27 16:54:29 -0300

    refactored entities

--- lib/xclarity_client/xclarity_resource.rb
+++ lib/xclarity_client/xclarity_resource.rb
@@ -4,6 +4,7 @@ module XClarityClient
       attributes.each do |key, value|
         begin
           value = value.gsub("\u0000", '') if value.is_a?(String)
+          key = key.gsub("-","_")
           send("#{key}=", value)
         rescue
           $log.warn("UNEXISTING ATTRIBUTES FOR #{self.class}: #{key}") unless defined?(Rails).nil?
