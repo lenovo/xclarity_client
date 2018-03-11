84a797f1ad79d3491339c28358cec52f8c50bd7f
douglasgabriel <douglasxgabriel@gmail.com>
2018-03-11 17:25:57 -0300

    refactoring errors classes organization

---
+++ lib/xclarity_client/errors/errors.rb
@@ -0,0 +1,10 @@
+module XClarityClient
+  module Errors
+  end
+end
+
+require 'xclarity_client/errors/authentication_error'
+require 'xclarity_client/errors/connection_failed'
+require 'xclarity_client/errors/connection_failed_unknown'
+require 'xclarity_client/errors/connection_refused'
+require 'xclarity_client/errors/hostname_unknown'
