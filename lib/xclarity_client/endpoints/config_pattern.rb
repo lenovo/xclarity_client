287dde45a4c3bb254a2c6161130eb48c1e07486c
douglasgabriel <douglasxgabriel@gmail.com>
2018-03-11 16:49:59 -0300

    putting endpoints classes into 'endpoints' folder

--- lib/xclarity_client/endpoints/config_pattern.rb
+++ lib/xclarity_client/endpoints/config_pattern.rb
@@ -6,7 +6,7 @@ module XClarityClient
     LIST_NAME = 'items'.freeze
 
     attr_accessor :identifier, :items, :label, :id, :referencedBy, :userDefined, :formFactor, :inUse, :description, :name, :nodeType, :type,
-    :uri, :serverType, :template_type, :server_template, :storageSettings, :bootSettings, :adapterSettings, 
+    :uri, :serverType, :template_type, :server_template, :storageSettings, :bootSettings, :adapterSettings,
     :templates, :sub_templates, :useCount
 
     def initialize(attributes)
