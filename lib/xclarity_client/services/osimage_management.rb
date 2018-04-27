9c0443661a16df31abb2ca2b3a5d687b519166c5
felipedf <felipe.diogenes13@gmail.com>
2018-04-27 17:20:38 -0300

    Identing better few files to reduce our lint issues

--- lib/xclarity_client/services/osimage_management.rb
+++ lib/xclarity_client/services/osimage_management.rb
@@ -14,34 +14,32 @@ module XClarityClient
       get_all_resources(OsImage)
     end
 
-    def import_osimage(serverId, path)
-        if not Schemas.validate_input_parameter("serverId", serverId, String)
-           return
-        end
+    def import_osimage(server_id, path)
+      return unless Schemas.validate_input_parameter("serverId", server_id, String) ||
+                    Schemas.validate_input_parameter("path", path, String)
 
-        if not Schemas.validate_input_parameter("path", path, String)
-           return
-        end
- 
-        msg="inputs serverId=#{serverId},path=#{path}"
-        $lxca_log.info self.class.to_s+" "+__method__.to_s, msg
-        opts = { :Action => "Init" }
-        request_body = JSON.generate(opts)
-        response = do_post("#{OsImage::BASE_URI}?imageType=OS", request_body)
-        response = JSON.load(response.body).to_hash
-        jobId = response["jobId"]
-        puts jobId
-        opts = { :serverId => serverId, :path => path }
-        request_body = JSON.generate(opts)
-        imageName = path.split(File::SEPARATOR).last
-        begin
-           response = do_post("#{OsImage::BASE_URI}/?jobId=#{jobId}"\
-                              "&imageType=OS&imageName=#{imageName}", request_body)
-           response = JSON.parse(response.body)
-        rescue Faraday::TimeoutError => e
-           result = JSON.generate({ :result => " Job for importing image is in progress with jobId #{jobId}"})
-           response = JSON.parse(result) 
-        end
+      msg="inputs serverId=#{server_id},path=#{path}"
+      $lxca_log.info self.class.to_s+" "+__method__.to_s, msg
+      opts = { :Action => "Init" }
+      request_body = JSON.generate(opts)
+      response = do_post("#{OsImage::BASE_URI}?imageType=OS", request_body)
+      response = JSON.load(response.body).to_hash
+      job_id = response["jobId"]
+      opts = { :serverId => server_id, :path => path }
+      request_body = JSON.generate(opts)
+      image_name = path.split(File::SEPARATOR).last
+      begin
+        response = do_post(
+          "#{OsImage::BASE_URI}/?jobId=#{job_id}&imageType=OS&imageName=#{image_name}",
+          request_body
+        )
+        JSON.parse(response.body)
+      rescue Faraday::TimeoutError => e
+        result = JSON.generate(
+          :result => " Job for importing image is in progress with jobId #{job_id}"
+        )
+        JSON.parse(result)
+      end
     end
   end
 end
