ebcaf5bf0d6148d9286bf839cc242743023c1cc5
umrikar <raghav40_it@yahoo.co.in>
2018-06-11 16:26:06 +0530

    Update osimage_management.rb

--- lib/xclarity_client/services/osimage_management.rb
+++ lib/xclarity_client/services/osimage_management.rb
@@ -1,183 +1,33 @@
-require 'json-schema'
+require 'json'
+require 'pathname'
 
 module XClarityClient
-  class Schemas
-    @remotefs = {
-      "id"         => "create_remotefileserver_profile",
-      "type"       => "object",
-      "required"   => %w(address displayName port protocol),
-      "properties" => {
-        "address"      => {
-          "type" => "string"
-        },
-        "displayName"  => {
-          "type" => "string"
-        },
-        "keyComment"   => {
-          "type" => "string"
-        },
-        "keyPassphras" => {
-          "type" => "string"
-        },
-        "keyType"      => {
-          "type" => "string"
-        },
-        "password"     => {
-          "type" => "string"
-        },
-        "port"         => {
-          "type" => "integer"
-        },
-        "protocol"     => {
-          "type" => "string"
-        },
-        "serverId"     => {
-          "type" => "string"
-        },
-        "username"     => {
-          "type" => "string"
+  class OsImageManagement < Services::XClarityService
+    manages_endpoint OsImage
+
+    def import_osimage(server_id, path)
+      msg="inputs serverId=#{server_id},path=#{path}"
+      $lxca_log.info self.class.to_s+" "+__method__.to_s, msg
+      opts = { :Action => "Init" }
+      request_body = JSON.generate(opts)
+      response = @connection.do_post("#{OsImage::BASE_URI}?imageType=OS",
+                                     request_body)
+      response = JSON.load(response.body).to_hash
+      job_id = response["jobId"]
+      opts = { :serverId => server_id, :path => path }
+      request_body = JSON.generate(opts)
+      image_name = path.split(File::SEPARATOR).last
+      begin
+        response = @connection.do_post("#{OsImage::BASE_URI}?jobId=#{job_id}"\
+                                       "&imageType=OS&imageName=#{image_name}",
+                                       request_body)
+        response
+      rescue Faraday::TimeoutError => e
+        result = {
+          :result => "importing image is in progress with jobId #{job_id}"
         }
-      }
-    }
-
-    @hostplatforms = {
-      "id"    => "deploy_osimage",
-      "type"  => "array",
-      "items" => {
-        "type"       => "object",
-        "required"   => %w(networkSettings selectedImage storageSettings uuid),
-        "properties" => {
-          "adusername"        => {
-            "type" => "string"
-          },
-          "adpassword"        => {
-            "type" => "string"
-          },
-          "configFileId"      => {
-            "type" => "string"
-          },
-          "licenseKey"        => {
-            "type" => "string"
-          },
-          "selectedImage"     => {
-            "type" => "string"
-          },
-          "storageSettings"   => {
-            "type" => "object"
-          },
-          "unattendFileId"    => {
-            "type" => "string"
-          },
-          "uuid"              => {
-            "type" => "string"
-          },
-          "windowsDomain"     => {
-            "type" => "string"
-          },
-          "windowsDomainBlob" => {
-            "type" => "string"
-          },
-          "networkSettings"   => {
-            "type"       => "object",
-            "properties" => {
-              "dns1"         => {
-                "type" => "string"
-              },
-              "dns2"         => {
-                "type" => "string"
-              },
-              "gateway"      => {
-                "type" => "string"
-              },
-              "hostname"     => {
-                "type" => "string"
-              },
-              "ipAddress"    => {
-                "type" => "string"
-              },
-              "mtu"          => {
-                "type" => "integer"
-              },
-              "prefixLength" => {
-                "type" => "integer"
-              },
-              "selectedMAC"  => {
-                "type" => "string"
-              },
-              "vlanId"       => {
-                "type" => "integer"
-              }
-            }
-          }
-        }
-      }
-    }
-
-    @globalsettings = {
-      "id"         => "set_globalsettings",
-      "type"       => "object",
-      "required"   => %w(activeDirectory
-                         ipAssignment
-                         isVLANMode
-                         licenseKeys
-                         credentials),
-      "properties" => {
-        "activeDirectory" => {
-          "type" => "object"
-        },
-        "ipAssignment"    => {
-          "type" => "string"
-        },
-        "isVLANMode"      => {
-          "type" => "boolean"
-        },
-        "licenseKeys"     => {
-          "type" => "object"
-        },
-        "deploySettings"  => {
-          "type" => "string"
-        },
-        "credentials"     => {
-          "type"  => "array",
-          "items" => {
-            "type"       => "object",
-            "required"   => %w(type name password passwordChanged),
-            "properties" => {
-              "type"            => {
-                "type" => "string"
-              },
-              "name"            => {
-                "type" => "string"
-              },
-              "password"        => {
-                "type" => "string"
-              },
-              "passwordChanged" => {
-                "type" => "boolean"
-              }
-            }
-          }
-        }
-      }
-    }
-
-    REQ_SCHEMA = {
-      :deploy_osimage                  => @hostplatforms,
-      :set_globalsettings              => @globalsettings,
-      :create_remotefileserver_profile => @remotefs
-    }.freeze
-
-    def self.validate_input(schema_name, data)
-      x = JSON::Validator.fully_validate(self::REQ_SCHEMA[schema_name], data)
-      unless x.empty?
-        errmsg = "input validation failed for data #{data}"
-        $lxca_log.error('XClarityClient::Schemas validate_input', errmsg.to_s)
-        g = ''
-        x.each do |k|
-          $lxca_log.error('XClarityClient::Schemas validate_input', k)
-          g << "#{k},  "
-        end; return { :result => 'Input validation failed', :message => g }
-      end; return { :result => 'success' }
+        result
+      end
     end
   end
 end
