9379885148a758ef379dea0b81e615dfa8988af0
rumrikar <rumrikar@lenovo.com>
2018-08-24 15:25:22 +0530

    Add update repository functionality

--- lib/xclarity_client/mixins/update_repo_mixin.rb
+++ lib/xclarity_client/mixins/update_repo_mixin.rb
@@ -6,5 +6,28 @@ module XClarityClient
     def discover_update_repo(opts = {})
       UpdateRepoManagement.new(@config).fetch_all(opts)
     end
+
+    def read_update_repo
+      UpdateRepoManagement.new(@config).read_update_repo
+    end
+
+    def refresh_update_repo(scope, mt, os)
+      UpdateRepoManagement.new(@config).refresh_update_repo(scope, mt, os)
+    end
+
+    def acquire_firmware_updates(scope, fixids, mt)
+      UpdateRepoManagement.new(@config).acquire_firmware_updates(scope,
+                                                                 fixids, mt)
+    end
+
+    def delete_firmware_updates(file_types, fixids)
+      UpdateRepoManagement.new(@config).delete_firmware_updates(file_types,
+                                                                fixids)
+    end
+
+    def export_firmware_updates(file_types, fixids)
+      UpdateRepoManagement.new(@config).export_firmware_updates(file_types,
+                                                                fixids)
+    end
   end
 end
--- lib/xclarity_client/services/update_repo_management.rb
+++ lib/xclarity_client/services/update_repo_management.rb
@@ -33,5 +33,63 @@ module XClarityClient
         updatesByMtByComp
       )
     end
+
+    public
+
+    def check_file_types(file_types)
+      x = 'Invalid value for argument file_types. Allowed values are'\
+               + ' - all and payloads'
+      raise x unless file_types.casecmp('payloads').zero? ||
+                     file_types.casecmp('all').zero?
+    end
+
+    def read_update_repo
+      response = @connection.do_put(UpdateRepo::BASE_URI + '?action=read')
+      response.body
+    end
+
+    def refresh_update_repo(scope, mt, os)
+      if scope.casecmp('all') != 0 && scope.casecmp('latest') != 0
+        raise 'Invalid argument combination of action and scope. Action'\
+               + ' refresh can have scope as either all or latest'
+      end
+      refresh_req = JSON.generate(:mt => mt, :os => os, :type => 'catalog')
+      response = @connection.do_put(UpdateRepo::BASE_URI\
+                                    + '?action=refresh&with='\
+                                    + scope.downcase, refresh_req)
+      response.body
+    end
+
+    def acquire_firmware_updates(scope, fixids, mt)
+      if scope.casecmp('payloads') != 0
+        raise 'Invalid argument combination of action and scope. Action'\
+               + ' acquire can have scope as payloads'
+      end
+      acquire_req = JSON.generate(:mt => mt, :fixids => fixids,
+                                  :type => 'latest')
+      response = @connection.do_put(UpdateRepo::BASE_URI\
+                                    + '?action=acquire&with='\
+                                    + scope.downcase, acquire_req)
+      response.body
+    end
+
+    def delete_firmware_updates(file_types, fixids)
+      check_file_types(file_types)
+      delete_req = JSON.generate(:fixids => fixids)
+      response = @connection.do_put(UpdateRepo::BASE_URI + '?action='\
+                                    + 'delete&filetypes=' + file_types.downcase,
+                                    delete_req)
+      response.body
+    end
+
+    def export_firmware_updates(file_types, fixids)
+      check_file_types(file_types)
+
+      export_req = JSON.generate(:fixids => fixids)
+      response = @connection.do_put(UpdateRepo::BASE_URI\
+                                    + '?action=export&filetypes='\
+                                    + file_types.downcase, export_req)
+      response.body
+    end
   end
 end
