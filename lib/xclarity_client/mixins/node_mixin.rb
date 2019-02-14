bf2999fec32afeae836c9116f8f4b740bb04d25c
rumrikar <rumrikar@lenovo.com>
2019-02-14 15:53:24 +0530

    Added support for media mount

--- lib/xclarity_client/mixins/node_mixin.rb
+++ lib/xclarity_client/mixins/node_mixin.rb
@@ -57,7 +57,66 @@ module XClarityClient
       node_management.set_power_state(uuid, :bootToF1)
     end
 
+    def retrieve_mounted_media_details(uuid = '')
+      node_management.retrieve_mounted_media_details(uuid)
+    end
+
+    def enable_media_mount_support_thinkserver(uuid = '')
+      node_management.enable_media_mount_support(uuid)
+    end
+
+    def disable_media_mount_support_thinkserver(uuid = '')
+      node_management.disable_media_mount_support(uuid)
+    end
+
+    def remove_all_mounted_medias_thinksystem(uuid = '')
+      node_management.remove_all_mounted_medias(uuid)
+    end
+
+    def mount_media_thinkserver(uuid, opts)
+      validate_mount_media_params(opts)
+      node_management.mount_media(uuid, opts)
+    end
+
+    def mount_media_thinksystem(uuid, opts)
+      validate_mount_media_params(opts, 'thinksystem')
+      node_management.mount_media(uuid, opts)
+    end
+
+    def unmount_media_thinkserver(uuid, media_uid)
+      node_management.unmount_media(uuid, media_uid)
+    end
+
+    def unmount_media_thinksystem(uuid, media_uid, media_type)
+      node_management.unmount_media(uuid, media_uid, media_type)
+    end
+
     private
+
+    def validate_mount_media_params(opts, server_type = '')
+      msg = 'parameter opts should be of type hash, below are valid fields '\
+            + "of hash \n\n"
+      msg_params =  ":mediaLocation      => Full path of media iso\n"\
+                  + ":mediaServerAddress => Ip Address of the server on\n"\
+                  + "                        which media is located\n"\
+                  + ":mediaType => (for ThinkServersOnly) The media type\n"\
+                  + "               this can be one of following\n"\
+                  + "  1) CD/DVD - CD drive\n"\
+                  + "  2) FD - flash drive\n  3) HD - disk drive\n"\
+                  + ":shareType => share type can be one of following \n"\
+                  + "  1) ftp\n  2) http\n  3) sftp\n  4) https\n"\
+                  + "  5) nfs\n  6) samba\n"\
+                  + ":username  => Required if shareType is samba,\n"\
+                  + "              User name to authenticate media\n"\
+                  + ":password  => Required if shareType is samba,\n"\
+                  + '              password to authenticate media'
+      err_msg = msg + msg_params
+      raise(err_msg) unless opts.kind_of?(Hash)
+
+      err_msg = ':mediaType is mandatory field in input hash for thinksystem'
+      raise(err_msg) if server_type == 'thinksystem' &&
+                     !opts.keys.include?(:mediaType)
+    end
 
     def node_management
       NodeManagement.new(@config)
--- lib/xclarity_client/services/node_management.rb
+++ lib/xclarity_client/services/node_management.rb
@@ -20,5 +20,42 @@ module XClarityClient
 
       send_power_request(managed_resource::BASE_URI + '/' + uuid + '/bmc', requested_state)
     end
+
+    def retrieve_mounted_media_details(uuid)
+      uri = Node::BASE_URI + "/#{uuid}/" + 'mediaMount'
+      @connection.do_get(uri)
+    end
+
+    def enable_media_mount_support(uuid)
+      uri = Node::BASE_URI + "/#{uuid}/" + 'mediaMount'
+      req_body = JSON.generate(:action => 'enableMountMedia')
+      @connection.do_put(uri, req_body)
+    end
+
+    def disable_media_mount_support(uuid)
+      uri = Node::BASE_URI + "/#{uuid}/" + 'mediaMount'
+      req_body = JSON.generate(:action => 'disableMountMedia')
+      @connection.do_put(uri, req_body)
+    end
+
+    def remove_all_mounted_medias(uuid)
+      uri = Node::BASE_URI + "/#{uuid}/" + 'mediaMount'
+      req_body = JSON.generate(:action => 'reset')
+      @connection.do_put(uri, req_body)
+    end
+
+    def mount_media(uuid, opts)
+      uri = Node::BASE_URI + "/#{uuid}/" + 'mediaMount'
+      req_body = JSON.generate(opts)
+      @connection.do_put(uri, req_body)
+    end
+
+    def unmount_media(uuid, media_uid, media_type = '')
+      uri = Node::BASE_URI + "/#{uuid}/" + 'mediaMount'
+      opts = { :action => 'unmount', :UID => media_uid,
+               :mediaType => media_type }
+      req_body = JSON.generate(opts)
+      @connection.do_put(uri, req_body)
+    end
   end
 end
